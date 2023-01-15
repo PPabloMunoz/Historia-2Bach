94b5743131d705c3b1982005e5231bdbeca70550
Pablo <pablomunoz.0507@gmail.com>
2023-01-15 22:23:15 +0100

    Vocabulario

---
+++ utils/dataVocabulario.ts
@@ -0,0 +1,54 @@
+import { Vocabulario } from '@/types'
+
+export const vocBloques1_2 = [
+  {
+    id: 1,
+    name: 'Atapuerca',
+    description:
+      'Yacimiento declarado Patrimonio de la Humanidad situado en la provincia de Burgos. Se descubrieron los restos el Homo antecessor. Multitud de restos datables en 430.000 BP'
+  },
+  {
+    id: 2,
+    name: 'Pueblos prerromanos',
+    description:
+      'Pueblos indígenas de las distintas zonas por las que se extendió el Imperio romano'
+  },
+  {
+    id: 3,
+    name: 'Tartesos',
+    description:
+      'Primeros hispánicos que se relacionaron con los pueblos históricos civilizados del Mediterráneo oriental'
+  },
+  {
+    id: 4,
+    name: 'Romanización',
+    description:
+      'Proceso histórico por el cual la población indígena de Hispania asimiló los modos de vida romanos económicos, sociales, políticos, culturales y aprendieron el latín'
+  },
+  {
+    id: 5,
+    name: 'Invasiones bárbaras',
+    description:
+      'Ocupaciones debilitaron e hicieron caer al Imperio Romano en el siglo V.d C'
+  },
+  {
+    id: 6,
+    name: 'Concilios de Toledo',
+    description:
+      'reuniones de clérigos, nobles y altos funcionarios durante el reino visigodo donde el rey proponía medidas administrativas y religiosas'
+  },
+  {
+    id: 7,
+    name: 'Recaredo',
+    description:
+      'Rey de los visigodos desde 586 hasta 601. El hecho más destacado cuando convocó el III Concilio de Toledo'
+  }
+]
+
+export const vocBloques3_4: Vocabulario[] = [
+  {
+    id: 1,
+    name: '',
+    description: ''
+  }
+]
