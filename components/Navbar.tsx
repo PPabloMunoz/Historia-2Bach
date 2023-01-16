c001340cd1943fc3eff8a61434bd27cd6f9376a4
Pablo <pablomunoz.0507@gmail.com>
2023-01-16 20:56:30 +0100

    navbar links and types

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,35 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function Navbar({ title = 'Historia 2º Bach' }) {
+export default function Navbar({
+  title = 'Historia 2º Bach',
+  home
+}: {
+  title?: String
+  home: Boolean
+}) {
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-      <nav className='flex flex-row justify-start items-center h-20 text-black font-bold px-20 text-xl bg-slate-300'>
+      <nav className='flex flex-row justify-around items-center h-20 text-black font-bold text-xl bg-slate-300'>
         <Link href='/'>Historia 2º Bach</Link>
+        {!home && (
+          <div>
+            <ul className='flex flex-row justify-center items-center gap-4 font-medium text-lg'>
+              <li>
+                <Link href='vocabulario'>Vocabulario</Link>
+              </li>
+              <li>
+                <Link href='cronologia'>Cronología</Link>
+              </li>
+              <li>
+                <Link href='ejercicios'>Ejercicios</Link>
+              </li>
+            </ul>
+          </div>
+        )}
       </nav>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,52 @@
-import { cronoBloques1_2, cronoBloque2_3 } from '@/utils/dataCronologia'
-import { vocBloques1_2, vocBloques3_4 } from '@/utils/dataVocabulario'
+import {
+  cronoBloques1_2,
+  cronoBloque3_4,
+  cronoBloque5_6
+} from '@/utils/dataCronologia'
+import {
+  vocBloque1,
+  vocBloque2,
+  vocBloque3,
+  vocBloque4,
+  vocBloque5
+} from '@/utils/dataVocabulario'
+
+import Link from 'next/link'
+import Navbar from '@/components/Navbar'
+import { BiUpArrowAlt } from 'react-icons/bi'
+
+import buttonStyles from '@/styles/button.module.css'
 
 export default function Home() {
   return (
     <>
-      <h1 className='text-4xl font-bold text-center'>Historia 2BACH</h1>
+      <Navbar home={true} />
+      <h1 className='text-5xl font-bold text-center mt-6 mb-20'>
+        Historia 2º Bach
+      </h1>
+      <p className='font-semibold text-center text-4xl mb-32'>
+        Entra en una de nuestras páginas para empezar a aprender y practicar
+      </p>
+      <div className='flex flex-row justify-around items-center w-full'>
+        <div className='flex flex-col justify-center items-center gap-10 w-1/4'>
+          <Link href='/vocabulario' className={buttonStyles.button}>
+            Vocabulario
+          </Link>
+          <BiUpArrowAlt size={140} />
+        </div>
+        <div className='flex flex-col justify-center items-center gap-10 w-1/4'>
+          <Link href='/cronologia' className={buttonStyles.button}>
+            Cronología
+          </Link>
+          <BiUpArrowAlt size={140} />
+        </div>
+        <div className='flex flex-col justify-center items-center gap-10 w-1/4'>
+          <Link href='/ejercicios' className={buttonStyles.button}>
+            Ejercicios
+          </Link>
+          <BiUpArrowAlt size={140} />
+        </div>
+      </div>
     </>
   )
 }
