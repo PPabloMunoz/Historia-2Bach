7abee5222124b47b9a9b31b0188d4698a6547811
Pablo <pablomunoz.0507@gmail.com>
2023-01-16 19:22:34 +0100

    cronologia page

---
+++ pages/cronologia.tsx
@@ -0,0 +1,10 @@
+import Navbar from '@/components/Navbar'
+
+export default function Cronologia() {
+  return (
+    <>
+      <Navbar title='Cronología - Historia 2º Bach' />
+      <h1>Cronología</h1>
+    </>
+  )
+}
