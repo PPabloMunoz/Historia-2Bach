52020567d9f9993617cc82cd727e8771ad1f5a71
Pablo <pablomunoz.0507@gmail.com>
2023-01-20 07:41:29 +0100

    supabase Client

---
+++ supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://bwcnfcsmejfezpfaqrud.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3Y25mY3NtZWpmZXpwZmFxcnVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQxNTI3NDEsImV4cCI6MTk4OTcyODc0MX0.ww8Bla3WvOkkHdIXR3__aTakv0XLhnl5uGuC2btmZgc'
+)
