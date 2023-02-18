0dc2c59d0f9d5b0abaedb0c5a3cbc8890b0cad2d
Pablo <pablomunoz.0507@gmail.com>
2023-02-18 13:43:55 +0100

    destrucure URL and KEY for supabase

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './supabase'
 
-export const supabase = createClient<Database>(
-  'https://bwcnfcsmejfezpfaqrud.supabase.co',
+const supabaseUrl = 'https://bwcnfcsmejfezpfaqrud.supabase.co'
+const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3Y25mY3NtZWpmZXpwZmFxcnVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQxNTI3NDEsImV4cCI6MTk4OTcyODc0MX0.ww8Bla3WvOkkHdIXR3__aTakv0XLhnl5uGuC2btmZgc'
-)
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
