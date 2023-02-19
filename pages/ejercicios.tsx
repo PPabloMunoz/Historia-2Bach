d9a18c79f4d7442017b637c3c13edb5fa3a166db
Pablo <pablomunoz.0507@gmail.com>
2023-02-19 22:57:18 +0100

    ejercicios page loading

--- pages/ejercicios.tsx
+++ pages/ejercicios.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import {
   cronoBloques1_2,
   cronoBloque3_4,
@@ -6,7 +7,8 @@ import {
 import { Cronologia } from '@/types'
 
 import Navbar from '@/components/Navbar'
-import { useEffect, useState } from 'react'
+import loading from '../public/loading.gif'
+import { useUserContext } from '@/utils/UserContext'
 
 function aleatory(data: Cronologia[], iterations: Number) {
   const random: Cronologia[] = []
@@ -34,18 +36,30 @@ function aleatory(data: Cronologia[], iterations: Number) {
 
 export default function CronologiaPage() {
   const [data, setData] = useState(cronoBloques1_2)
+  const { theme } = useUserContext()
+
   useEffect(() => {
     setData(aleatory(cronoBloque3_4, 6))
   }, [])
+
   return (
-    <>
+    <div
+      className={
+        theme === 'dark' ? 'dark bg-neutral-800 text-white min-h-screen' : ''
+      }
+    >
       <Navbar
         title='Cronología - Historia 2º Bach'
         home={false}
         page='ejercicios'
       />
-      <h1>Ejercicios</h1>
-      {data.map((item) => item.name)}
-    </>
+      <h1 className='text-5xl font-bold text-center mt-6 mb-20'>Ejercicios</h1>
+      <div className='text-center text-5xl flex flex-col justify-center items-center gap-32 '>
+        <p>Disponible dentro de poco</p>
+        <img src={loading.src} alt='my-gif' />
+      </div>
+
+      {/* {data.map((item) => item.name)} */}
+    </div>
   )
 }
