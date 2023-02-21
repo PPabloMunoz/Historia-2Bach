573f4f1fe379218c0001fc1f1ed75802b1e832ee
Pablo <pablomunoz.0507@gmail.com>
2023-02-21 16:07:03 +0100

    sort array function

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,38 +1,78 @@
 import { Cronologia } from '@/types'
 import { supabase } from '@/supabaseClient'
 
-export function aleatoryFunction(array: Array<Cronologia>, iterations: number) {
-  if (iterations > array.length) throw new Error('Demasiadas iteraciones')
+export async function aleatoryFunction(
+  blockNum: number,
+  iterations: number,
+  setFunction: Function
+) {
+  const array: Array<Cronologia> = await getData(blockNum)
+  if (await array) {
+    if (iterations > array.length) throw new Error('Demasiadas iteraciones')
 
-  const random: Array<Cronologia> = []
-  let temp: Array<Cronologia> = array
-  temp = temp.filter((item: Cronologia) => item !== undefined)
+    const random: Array<Cronologia> = []
+    let temp: Array<Cronologia> = array
+    temp = temp.filter((item: Cronologia) => item !== undefined)
 
-  for (let i = 0; i < iterations; i++) {
-    let rand = Math.floor(Math.random() * temp.length)
-    let rValue: Cronologia = temp[rand]
+    for (let i = 0; i < iterations; i++) {
+      let rand = Math.floor(Math.random() * temp.length)
+      let rValue: Cronologia = temp[rand]
 
-    while (random.includes(rValue)) {
-      rand = Math.floor(Math.random() * temp.length)
-      rValue = temp[rand]
+      while (random.includes(rValue)) {
+        rand = Math.floor(Math.random() * temp.length)
+        rValue = temp[rand]
+      }
+      random.push(rValue)
+
+      temp = temp.filter((e) => {
+        return e !== rValue
+      })
     }
-    random.push(rValue)
 
-    temp = temp.filter((e) => {
-      return e !== rValue
-    })
+    setFunction(random)
+    return random
   }
-
-  console.log(random)
-
-  return random
 }
 
-export async function getData(block: string, setFunction: Function) {
-  const { data, error } = await supabase.from(block).select()
+async function getData(block: number) {
+  const blockString = getBlockName(block)
+  const { data, error } = await supabase.from(blockString!).select()
   if (error) {
-    console.error('An error ocurred: ', error.message)
+    throw new Error(`An error ocurren ${error.message}`)
   }
-  setFunction(data)
   return data
+}
+
+function getBlockName(block: number) {
+  if (block === 1) {
+    return 'cronoBloques1_2'
+  } else if (block === 2) {
+    return 'cronoBloques3_4'
+  } else if (block === 3) {
+    return 'cronoBloques5_8'
+  } else {
+    throw new Error('Invalid Block')
+  }
+}
+
+export async function sortArray(
+  aleatoryArray: Array<Cronologia>,
+  blockNum: number,
+  setFunction: Function
+) {
+  const finalArray: Array<Cronologia> = []
+  const data: Array<Cronologia> = await getData(blockNum)
+  if (data && aleatoryArray) {
+    for (let i = 0; i < data.length; i++) {
+      for (let m = 0; m < aleatoryArray.length; m++) {
+        if (data[i].id === aleatoryArray[m].id) {
+          finalArray.push(aleatoryArray[m])
+          break
+        }
+      }
+    }
+  }
+
+  setFunction(finalArray)
+  return finalArray
 }
