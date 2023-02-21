aeb0bc813ddb3ea8f20219c318fa2b8fd2fb923d
Pablo <pablomunoz.0507@gmail.com>
2023-02-21 16:32:14 +0100

    aleatory and sort words ejercicios page

--- pages/ejercicios.tsx
+++ pages/ejercicios.tsx
@@ -1,42 +1,29 @@
-// import { useState, useEffect } from 'react'
-import Image from 'next/image'
+import { useState, useEffect } from 'react'
 
 import { useUserContext } from '@/utils/UserContext'
+import { aleatoryFunction, sortArray } from '@/utils/functions'
 
 import Navbar from '@/components/Navbar'
-import loading from '../public/loading.gif'
-
-// function aleatory(data: Cronologia[], iterations: Number) {
-//   const random: Cronologia[] = []
-//   let temp = data
-//   temp = temp.filter((item) => item !== undefined)
-
-//   for (let i = 0; i < iterations; i++) {
-//     let rand = Math.floor(Math.random() * temp.length)
-//     let rValue: Cronologia = temp[rand]
-//     console.log(rValue, 'value')
-
-//     while (random.includes(rValue)) {
-//       rand = Math.floor(Math.random() * temp.length)
-//       rValue = temp[rand]
-//     }
-//     random.push(rValue)
-
-//     temp = temp.filter((e) => {
-//       return e !== rValue
-//     })
-//   }
-
-//   return random
-// }
+import { Cronologia } from '@/types'
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai'
 
 export default function CronologiaPage() {
-  // const [data, setData] = useState(cronoBloques1_2)
   const { theme } = useUserContext()
 
-  // useEffect(() => {
-  //   setData(aleatory(cronoBloque3_4, 6))
-  // }, [])
+  const [viewSolution, setViewSolution] = useState<Boolean>(false)
+  const [blockSelected, setBlockSelected] = useState<number>(1)
+  // const [data, setData] = useState<Array<Cronologia>>()
+  const [aleatory, setAleatory] = useState<Array<Cronologia>>()
+  const [mySolution, setMySolution] = useState<Array<Cronologia>>()
+  const [solution, setSolution] = useState<Array<Cronologia>>()
+
+  useEffect(() => {
+    aleatoryFunction(blockSelected, 6, setAleatory)
+  }, [blockSelected])
+
+  useEffect(() => {
+    sortArray(aleatory!, blockSelected, setSolution)
+  }, [aleatory, blockSelected])
 
   return (
     <div
@@ -49,16 +36,68 @@ export default function CronologiaPage() {
         home={false}
         page='ejercicios'
       />
-      <h1 className='text-5xl font-bold text-center mt-6 mb-20'>Ejercicios</h1>
-      <div className='text-center text-5xl flex flex-col justify-center items-center gap-32 '>
-        <p>Disponible dentro de poco</p>
-        <Image
-          src={loading.src}
-          alt='my-gif'
-          width={loading.width}
-          height={loading.height}
-        />
+      <h1 className='text-5xl font-bold text-center mt-6 mb-10'>Ejercicios</h1>
+      <div className='w-full flex flex-row justify-center items-center mb-10 gap-5'>
+        <select
+          id='countries'
+          className='max-w-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          defaultValue={1}
+          onChange={(e) => {
+            console.log(e.target.value, 'value')
+            console.log(typeof parseInt(e.target.value), ':type of value')
+            setBlockSelected(parseInt(e.target.value))
+          }}
+        >
+          <option value={1}>Bloques 1 y 2</option>
+          <option value={2}>Bloques 3 y 4</option>
+          <option value={3}>Bloques 5 al 8</option>
+        </select>
+        <button className='bg-purple-600 py-2 px-2 rounded-lg'>
+          Cambiar palabras
+        </button>
       </div>
+
+      <main className='flex flex-row justify-evenly items-start gap-5 container m-auto'>
+        <div className='flex flex-col justify-start items-center'>
+          <h2 className='font-bold text-xl text-center mb-7'>Palabras</h2>
+          <section className='flex flex-col justify-center items-center gap-2 min-w-[200px]'>
+            {aleatory?.map((item) => (
+              <p
+                key={item.id}
+                className='bg-blue-600 w-full text-center rounded-lg py-2 px-3 text-sm text-white dark:text-white'
+              >
+                {item.name}
+              </p>
+            ))}
+          </section>
+        </div>
+        <div>
+          <h2 className='font-bold text-xl text-center mb-7'>Mi Solución</h2>
+        </div>
+        <div className='relative'>
+          <h2 className='font-bold text-xl text-center mb-7'>Solucion</h2>
+          <section
+            className={`min-w-[200px] flex flex-col justify-center items-center gap-2 ${
+              !viewSolution && 'blur'
+            }`}
+          >
+            {solution?.map((item) => (
+              <p
+                key={item.id}
+                className='bg-blue-600 w-full text-center rounded-lg py-2 px-3 text-sm text-white dark:text-white'
+              >
+                {item.name}
+              </p>
+            ))}
+          </section>
+          <div
+            className='absolute top-0 right-0 bg-blue-600 px-2 py-2 rounded-lg text-xs cursor-pointer'
+            onClick={() => setViewSolution(!viewSolution)}
+          >
+            {viewSolution ? <AiFillEyeInvisible /> : <AiFillEye />}
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
