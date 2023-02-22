d2449caaefe3a97734b6c7e377698e4a4554ccbc
Pablo <pablomunoz.0507@gmail.com>
2023-02-22 22:00:46 +0100

    delete console.log

--- pages/ejercicios.tsx
+++ pages/ejercicios.tsx
@@ -28,7 +28,6 @@ export default function CronologiaPage() {
   useEffect(() => {
     function handleResize() {
       setWindowWidth(window.innerWidth)
-      console.log(window.innerWidth, 'width')
     }
 
     window.addEventListener('resize', handleResize)
