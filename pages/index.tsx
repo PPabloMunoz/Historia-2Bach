8ef0ca33b7652fafd4d9ce5734404247e6a75634
Pablo <pablomunoz.0507@gmail.com>
2023-02-22 07:23:08 +0100

    index responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,27 +16,36 @@ export default function Home() {
       }
     >
       <Navbar home />
-      <h1 className='text-6xl font-bold text-center mt-6 mb-20'>
+      <h1 className='text-3xl lg:text-6xl font-bold text-center mt-6 lg:mb-20'>
         Historia 2º Bach
       </h1>
-      <p className='font-semibold text-center text-4xl mb-32'>
+      <p className='font-semibold text-center text-xl lg:text-4xl my-9 lg:mb-32'>
         Entra en una de las páginas para empezar a aprender y practicar
       </p>
-      <div className='flex flex-row justify-around items-center w-full'>
+      <div className='flex flex-row justify-around items-center w-full mt-20 lg:mt-0'>
         <div className='flex flex-col justify-center items-center gap-10 w-1/4'>
-          <Link href='/vocabulario' className={buttonStyles.button}>
+          <Link
+            href='/vocabulario'
+            className={buttonStyles.button + ' text-sm lg:text-base'}
+          >
             Vocabulario
           </Link>
           <BiUpArrowAlt size={140} className='hidden sm:block' />
         </div>
         <div className='flex flex-col justify-center items-center gap-10 w-1/4'>
-          <Link href='/cronologia' className={buttonStyles.button}>
+          <Link
+            href='/cronologia'
+            className={buttonStyles.button + ' text-sm lg:text-base'}
+          >
             Cronología
           </Link>
           <BiUpArrowAlt size={140} className='hidden sm:block' />
         </div>
         <div className='flex flex-col justify-center items-center gap-10 w-1/4'>
-          <Link href='/ejercicios' className={buttonStyles.button}>
+          <Link
+            href='/ejercicios'
+            className={buttonStyles.button + ' text-sm lg:text-base'}
+          >
             Ejercicios
           </Link>
           <BiUpArrowAlt size={140} className='hidden sm:block' />
