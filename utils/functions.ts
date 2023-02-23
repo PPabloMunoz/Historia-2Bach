2629b7215a4d1e8ae6d2d809d36e3aaa85e849e0
Pablo <pablomunoz.0507@gmail.com>
2023-02-23 21:14:32 +0100

    factorize functions

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,12 +1,13 @@
 import { Cronologia } from '@/types'
 import { supabase } from '@/supabaseClient'
+import { cronologiaNames, vocabularioNames } from './names'
 
 export async function aleatoryFunction(
   blockNum: number,
   iterations: number,
   setFunction: Function
 ) {
-  const array: Array<Cronologia> = await getData(blockNum)
+  const array: Array<Cronologia> = await getData(blockNum, 'cronologia')
   if (await array) {
     if (iterations > array.length) throw new Error('Demasiadas iteraciones')
 
@@ -34,25 +35,42 @@ export async function aleatoryFunction(
   }
 }
 
-async function getData(block: number) {
-  const blockString = getBlockName(block)
+export async function getData(
+  block: number,
+  type: 'cronologia' | 'vocabulario',
+  setFunction?: Function
+) {
+  const blockString =
+    type === 'cronologia' ? getCronoBlockName(block) : getVocBlockName(block)
   const { data, error } = await supabase.from(blockString!).select()
   if (error) {
     throw new Error(`An error ocurren ${error.message}`)
   }
+
+  setFunction && setFunction(data)
+
   return data
 }
 
-function getBlockName(block: number) {
-  if (block === 1) {
-    return 'cronoBloques1_2'
-  } else if (block === 2) {
-    return 'cronoBloques3_4'
-  } else if (block === 3) {
-    return 'cronoBloques5_8'
-  } else {
-    throw new Error('Invalid Block')
-  }
+function getCronoBlockName(block: number) {
+  if (block === 1) return cronologiaNames.crono1
+  if (block === 2) return cronologiaNames.crono2
+  if (block === 3) return cronologiaNames.crono3
+
+  throw new Error('Invalid cronologia number block')
+}
+
+function getVocBlockName(block: number) {
+  if (block === 1) return vocabularioNames.voc1
+  if (block === 2) return vocabularioNames.voc2
+  if (block === 3) return vocabularioNames.voc3
+  if (block === 4) return vocabularioNames.voc4
+  if (block === 5) return vocabularioNames.voc5
+  if (block === 6) return vocabularioNames.voc6
+  if (block === 7) return vocabularioNames.voc7
+  if (block === 8) return vocabularioNames.voc8
+
+  throw new Error('Invalid vocabulario number block')
 }
 
 export async function sortArray(
@@ -61,7 +79,7 @@ export async function sortArray(
   setFunction: Function
 ) {
   const finalArray: Array<Cronologia> = []
-  const data: Array<Cronologia> = await getData(blockNum)
+  const data: Array<Cronologia> = await getData(blockNum, 'cronologia')
   if (data && aleatoryArray) {
     for (let i = 0; i < data.length; i++) {
       for (let m = 0; m < aleatoryArray.length; m++) {
