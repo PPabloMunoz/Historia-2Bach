266340980ce5746fb31f2b986f3763ca5ff85a69
Pablo <pablomunoz.0507@gmail.com>
2023-02-23 21:11:20 +0100

    block names file

---
+++ utils/names.ts
@@ -0,0 +1,18 @@
+const cronologiaNames = {
+  crono1: 'cronoBloques1_2',
+  crono2: 'cronoBloques3_4',
+  crono3: 'cronoBloques5_8'
+}
+
+const vocabularioNames = {
+  voc1: 'vocBloque1',
+  voc2: 'vocBloque2',
+  voc3: 'vocBloque3',
+  voc4: 'vocBloque4',
+  voc5: 'vocBloque5',
+  voc6: 'vocBloque6',
+  voc7: 'vocBloque7',
+  voc8: 'vocBloque8'
+}
+
+export { cronologiaNames, vocabularioNames }
